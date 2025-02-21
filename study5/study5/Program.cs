8d083c14532cfc90d8353b7eec40e74cc71fdb9d
jaejae1205 <163630615+jaejae1205@users.noreply.github.com>
2025-02-21 14:34:40 +0900

    study5

---
+++ study5/study5/Program.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace study5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //int integerNum = 10;
+            //float floatNum = 3.14f;
+            //double doubleNum = 3.14159;
+
+            //Console.WriteLine(integerNum);
+            //Console.WriteLine(floatNum);
+            //Console.WriteLine(doubleNum);
+
+            //int intValue = -100;
+            //long longValue = 1234567890L;
+
+            //Console.WriteLine(intValue);
+            //Console.WriteLine(longValue);
+
+            /*            sbyte signedByte = -50;
+                        short signedShort = -32000;
+                        int signedlnt = -2000000000;
+
+                        Console.WriteLine(signedByte);
+                        Console.WriteLine(signedShort);
+                        Console.WriteLine(signedlnt);*/
+
+            //byte unsignedByte = 255;
+            //ushort unsignedShort = 65000;
+            //uint unsignedInt = 400000000;
+
+            //Console.WriteLine(unsignedByte);
+            //Console.WriteLine(unsignedShort);
+            //Console.WriteLine(unsignedInt);
+
+            //float singlePrecision = 3.14f;
+            //double doublePrecision = 3.1415926535;
+            //decimal highPrecision = 3.141593292174591759123484933m;
+
+            //Console.WriteLine(singlePrecision);
+            //Console.WriteLine(doublePrecision);
+            //Console.WriteLine(highPrecision);
+
+            //int integerValue = 100;
+            //long longValue = 100L;
+            //float floatValue = 3.14f;
+            //double doubleValue = 3.14;
+            //decimal decimalValue = 3.14m;
+
+            //Console.WriteLine(integerValue);
+            //Console.WriteLine(longValue);
+            //Console.WriteLine(floatValue);
+            //Console.WriteLine(doubleValue);
+            //Console.WriteLine(decimalValue);
+
+            //char letter = 'A';
+            //char symbol = '#';
+            //char number = '9';
+
+            //Console.WriteLine(letter);
+            //Console.WriteLine(symbol);
+            //Console.WriteLine(number);
+
+            //string greeting = "Hello World!";
+            //string name = "Alice";
+
+            //Console.WriteLine(greeting);
+            //Console.WriteLine(name);
+
+            //Console.WriteLine("Hello World Alice");
+
+            //bool isRunning = true;
+            //bool isFinished = false;
+
+            //Console.WriteLine(isRunning);
+            //Console.WriteLine(isFinished);
+
+            //const double Pi = 3.14159;
+            //const int MaxScore = 100;
+
+            //Console.WriteLine(Pi);
+            //Console.WriteLine(MaxScore);
+
+            //System.Int32 number = 123;
+            //System.String text = "Hello";
+            //System.Boolean flag = true;
+
+            //Console.WriteLine(number);
+            //Console.WriteLine(text);
+            //Console.WriteLine(flag);
+
+            int number = 123;
+            string numberAsString = number.ToString();
+
+            bool flag = true;
+            string flagAsString = flag.ToString();
+
+            Console.WriteLine(numberAsString);
+            Console.WriteLine(flagAsString);
+
+
+        }
+    }
+}
