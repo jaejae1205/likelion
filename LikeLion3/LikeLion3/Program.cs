b144985a8d5ec41c028a8455a3ca2ae657556859
jaejae1205 <163630615+jaejae1205@users.noreply.github.com>
2025-02-21 13:04:16 +0900

    vs

---
+++ LikeLion3/LikeLion3/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int hp = 100;
+            double att = 56.7;
+            string name = "Alice";
+            char rank = 'S';
+                    
+            Console.WriteLine(hp);
+            Console.WriteLine(att);
+            Console.WriteLine(name);
+            Console.WriteLine(rank);
+
+        }
+    }
+}
---
+++ LikeLion4/LikeLion4/Program.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LikeLion4
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            /*            string greeting;
+                        greeting = "Hello, World!";
+
+                        Console.WriteLine(greeting);
+
+                        int score = 100;
+                        double temperature = 36.5;
+                        string city = "Seoul";
+
+                        Console.WriteLine(score);
+                        Console.WriteLine(temperature);
+                        Console.WriteLine(city);*/
+
+            //int x = 10, y = 20, z = 30;
+
+            //Console.WriteLine(x);
+            //Console.WriteLine(y);
+            //Console.WriteLine(z);
+
+            //const double Pi = 3.14159;
+            //const int MaxScore = 100;
+
+            //Console.WriteLine("Pi : " + Pi);
+            //Console.WriteLine("MaxScore : " + MaxScore);
+
+            int att = 16755;
+            int max_life = 78103;
+            int critical = 36;
+            int special = 1017;
+            int hold = 41;
+            int swift = 611;
+            int endure = 22;
+            int master = 39;
+
+            Console.WriteLine("공격력 : " + att);
+            Console.WriteLine("최대 생명력 : " + max_life);
+            Console.WriteLine("치명 : " + critical);
+            Console.WriteLine("특화 : " + special);
+            Console.WriteLine("제압 : " + hold);
+            Console.WriteLine("신속 : " + swift);
+            Console.WriteLine("인내 : " + endure);
+            Console.WriteLine("숙련 : " + master);
+
+
+        }
+    }
+}
