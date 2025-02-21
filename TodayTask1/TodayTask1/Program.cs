c0575abd2889b2f1240f5e8396b4d3439d45b2b1
jaejae1205 <163630615+jaejae1205@users.noreply.github.com>
2025-02-21 17:34:16 +0900

    Task1

---
+++ TodayTask1/TodayTask1/Program.cs
@@ -0,0 +1,71 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace TodayTask1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Console.Write("로딩바 엔터를 치면 스토리가 시작됩니다.. ");
+            Console.ReadLine();
+            Console.Clear();
+            Thread.Sleep(500);
+            Console.WriteLine("□□□□□");
+            Thread.Sleep(1000); //1000 이 1초다
+            Console.Clear();
+            Console.WriteLine("■□□□□");
+            Thread.Sleep(1000); //1000 이 1초다
+            Console.Clear();
+            Console.WriteLine("■■□□□");
+            Thread.Sleep(1000); //1000 이 1초다
+            Console.Clear();
+            Console.WriteLine("■■■□□");
+            Thread.Sleep(1000); //1000 이 1초다
+            Console.Clear();
+            Console.WriteLine("■■■■□");
+            Thread.Sleep(1000); //1000 이 1초다
+            Console.Clear();
+            Console.WriteLine("■■■■■");
+            Thread.Sleep(1000); //1000 이 1초다
+            Console.Clear();
+            Thread.Sleep(1000);
+             
+            Console.WriteLine("잠에서 깼을때 나는 무엇인가 다르다는 것을 느꼈다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("주변은 끝없이 펼쳐진 안개로 가득차 있었고 분위기가 심상치 않았다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("하지만 이 공간이 낯설게 느껴지지만은 않았다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("시선을 아래로 돌렸을 때, 내 손에는 검이 쥐어져 있었다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("그리고 큰 소리로 누군가의 목소리가 허공에 울려퍼졌다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("'소환사의 협곡에 오신 것을 환영합니다..'");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("..?");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("어떤 이유로 여기에 오게 된 건지, 내가 왜 이런 모습인 건지, 그건 알 수 없었다.");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("하지만 무엇을 해야 할지 알 것만 같았다..");
+            Console.ReadLine();
+            Console.Clear();
+            Console.WriteLine("'일단 물약부터..'");
+            Console.ReadLine();
+            Console.Clear();
+        }
+    }
+}
