6d27647572ef314611719036e4cd93eb6cfeff59
jaejae1205 <163630615+jaejae1205@users.noreply.github.com>
2025-02-21 15:04:18 +0900

    loading

---
+++ Loading/Loading/Program.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace Loading
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("□□□□□□□□□□");
+            Thread.Sleep(1000); //1000 이 1초다
+            Console.Clear();
+            Console.WriteLine("■□□□□□□□□□");
+            Thread.Sleep(1000); //1000 이 1초다
+            Console.Clear();
+            Console.WriteLine("■■□□□□□□□□");
+            Thread.Sleep(1000); //1000 이 1초다
+            Console.Clear();
+            Console.WriteLine("■■■□□□□□□□");
+            Thread.Sleep(1000); //1000 이 1초다
+            Console.Clear();
+            Console.WriteLine("■■■■□□□□□□");
+            Thread.Sleep(1000); //1000 이 1초다
+            Console.Clear();
+            Console.WriteLine("■■■■■□□□□□");
+            Thread.Sleep(1000); //1000 이 1초다
+            Console.Clear();
+            Console.WriteLine("■■■■■■□□□□");
+            Thread.Sleep(1000); //1000 이 1초다
+            Console.Clear();
+            Console.WriteLine("■■■■■■■□□□");
+            Thread.Sleep(1000); //1000 이 1초다
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■□□");
+            Thread.Sleep(1000); //1000 이 1초다
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■■□");
+            Thread.Sleep(1000); //1000 이 1초다
+            Console.Clear();
+            Console.WriteLine("■■■■■■■■■■");
+            Console.Clear();
+            Console.WriteLine("텍스트엘든링");
+
+            Console.WriteLine("스토리1");
+
+        }
+    }
+}
