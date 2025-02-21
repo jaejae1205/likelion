56def9e776145ebaf594921c640676435cc34821
jaejae1205 <163630615+jaejae1205@users.noreply.github.com>
2025-02-21 10:22:53 +0900

    study2

---
+++ likelion2/likelion2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace likelion2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int age;
+            age = 26;
+
+            Console.WriteLine(age);
+        }
+    }
+}
