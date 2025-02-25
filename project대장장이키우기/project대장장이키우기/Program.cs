5f3e69e80bf82446693650c3a89123367ee5c7c1
jaejae1205 <163630615+jaejae1205@users.noreply.github.com>
2025-02-25 15:10:04 +0900

    2.25

---
+++ project대장장이키우기/project대장장이키우기/Program.cs
@@ -0,0 +1,116 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+
+namespace project대장장이키우기
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Random rand = new Random();
+
+            Console.WriteLine(" 대장장이 키우기 ");
+            Thread.Sleep(1000);
+
+            int pmoney = 100;
+            int input;
+            int rnd;
+
+            Thread.Sleep(500);
+
+            while (true) //무한반복
+            {
+                Console.Clear();
+                Console.WriteLine("1. 나무캐기 ");
+                Console.WriteLine("2. 장비뽑기 ");
+                Console.WriteLine("3. 나가기 ");
+                Console.Write("입력 : ");
+                input = int.Parse(Console.ReadLine());
+
+                if (input == 1)
+                {
+                    while (true)
+                    {
+                        Console.WriteLine("나무캐기(엔터)");
+                        Console.WriteLine("뒤로가기 x");
+
+                        string str = Console.ReadLine();
+
+                        pmoney += 100;
+                        Console.WriteLine("소지금 : " + pmoney);
+                        if (str == "x")
+                        {
+                            Console.WriteLine("뒤로가기");
+                            break;
+                        }
+
+
+
+                    }
+                } //input1
+                else if (input == 2)
+                {
+
+                    if (pmoney >= 1000) // 돈이 있는지 확인 후 뽑기
+                    {
+                        pmoney -= 1000;
+
+                        for (int i = 1; i <= 20; i++)
+                        {
+
+                            rnd = rand.Next(1, 101);
+
+                            if (rnd == 1)
+                            {
+
+                                Console.WriteLine("도끼 등급 SSS");
+
+                            }
+                            else if (rnd >= 2 && rnd <= 6)
+                            {
+                                Console.WriteLine("도끼 등급 SS");
+                            }
+                            else if (rnd >= 7 && rnd <= 17)
+                            {
+                                Console.WriteLine("도끼 등급 S");
+                            }
+                            else if (rnd >= 18 && rnd <= 38)
+                            {
+                                Console.WriteLine("도끼 등급 A");
+                            }
+                            else if (rnd >= 39 && rnd <= 69)
+                            {
+                                Console.WriteLine("도끼 등급 B");
+                            }
+                            else
+                            {
+                                Console.WriteLine("도끼 등급 C");
+                            }
+                            Thread.Sleep(500);
+                        }
+                    }
+                    else
+                    {
+                        Console.WriteLine("돈이 부족합니다. \n");
+                        Thread.Sleep(1000);
+                    }
+                } //input2
+                else if (input == 3)
+                {
+                    Console.WriteLine("나갑니다");
+                    Thread.Sleep(1000);
+                    Environment.Exit(0);
+
+                } //input3
+
+            }
+
+
+
+        }
+    }
+}
---
+++ 모험가키우기/모험가키우기/Program.cs
@@ -0,0 +1,145 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Runtime.Remoting.Messaging;
+
+namespace 모험가키우기
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.OutputEncoding = System.Text.Encoding.UTF8;
+
+            Random rand = new Random();
+            int health = 100, gold = 700, power = 10;
+            bool isAlive = true;
+
+            Console.WriteLine(" 모험가 키우기 ");
+            Thread.Sleep(1000);
+
+            while (isAlive)
+            {
+                Console.Clear();
+                Console.WriteLine($"현재 상태: 체력 {health} | 골드 {gold} | 공격력 {power}");
+                Console.WriteLine("\n1. 탐험하기 ");
+                Console.WriteLine("2. 장비 뽑기 🎲 (1000골드)");
+                Console.WriteLine("3. 휴식하기 (체력 +20)");
+                Console.WriteLine("4. 게임 종료 ");
+                Console.Write("입력: ");
+
+                int input = int.Parse(Console.ReadLine());
+
+                if (input == 1)
+                {
+                    Console.Clear();
+                    Console.WriteLine(" 탐험을 떠납니다.");
+                    Thread.Sleep(500);
+                    Console.WriteLine(" 탐험을 떠납니다..");
+                    Thread.Sleep(500);
+                    Console.WriteLine(" 탐험을 떠납니다...");
+                    Thread.Sleep(500);
+                    Console.WriteLine(" 탐험을 떠납니다...");
+                    Thread.Sleep(500);
+                    Console.WriteLine(" 탐험을 떠납니다....");
+                    Thread.Sleep(1000);
+
+                    int eventChance = rand.Next(1, 101);
+
+                    if (eventChance <= 20)
+                    {
+                        int damage = rand.Next(5, 21);
+                        Console.WriteLine($" 몬스터를 만났습니다! (체력 -{damage})");
+                        health -= damage;
+                    }
+                    else if (eventChance <= 70)
+                    {
+                        int reward = rand.Next(100, 301);
+                        Console.WriteLine($"보물을 발견했습니다! (+{reward})");
+                        gold += reward;
+
+                    }
+                    else
+                    {
+                        int heal = rand.Next(10, 31);
+                        Console.WriteLine($" 신비한 약초를 발견했습니다! (+{heal})");
+                        health += heal;
+                    }
+
+                    if (health <= 0)
+                    {
+                        Console.WriteLine("\n체력이 0이 되어 사망했습니다.... 게임 오버");
+                        Thread.Sleep(1000);
+                        isAlive = false;
+                    }
+                    Thread.Sleep(3000);
+                }  //input 1
+                else if (input == 2)
+                {
+                    if (gold >= 1000)
+                    {
+                        gold -= 1000;
+                        Console.Clear();
+                        Console.WriteLine(" 장비를 뽑습니다...");
+                        Thread.Sleep(2000);
+
+                        int rnd = rand.Next(1, 101);
+
+                        if (rnd == 1)
+                        {
+                            Console.WriteLine("SSS급 전설의 검 (공격력 +50) 획득!");
+                            power += 50;
+                        }
+                        else if (rnd <= 10)
+                        {
+                            Console.WriteLine("SS급 전설의 검 (공격력 +30) 획득!");
+                            power += 30;
+                        }
+                        else if (rnd <= 30)
+                        {
+                            Console.WriteLine("S급 전설의 검 (공격력 +10) 획득!");
+                            power += 10;
+                        }
+                        else
+                        {
+                            Console.WriteLine("SS급 전설의 검 (공격력 +5) 획득!");
+                            power += 5;
+                        }
+                        Thread.Sleep(1000);
+                    }
+                    else
+                    {
+                        Console.WriteLine("골드가 부족합니다. 1000 골드 필요");
+                        Thread.Sleep(2000);
+                    }
+
+
+                } //input 2
+
+                else if (input == 3)
+                {
+                    Console.WriteLine("휴식을 취합니다...(+20 체력)");
+                    health += 20;
+                    Thread.Sleep(2000);
+
+                }  //input 3
+                else if (input == 4)
+                {
+                    Console.WriteLine("게임을 종료합니다..");
+                    Environment.Exit(0);
+                }
+                else
+                {
+                    Console.WriteLine("잘못된 입력입니다. 다시 선택하세요. ");
+                    Thread.Sleep(1000);
+                }
+                
+                    
+            }
+
+        }
+    }
+}
