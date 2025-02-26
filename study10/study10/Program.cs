ed41bab3fd51a7794b40ccdb8f7b28895ba2d4cd
jaejae1205 <163630615+jaejae1205@users.noreply.github.com>
2025-02-26 15:01:35 +0900

    2.26

--- study10/study10/Program.cs
+++ study10/study10/Program.cs
@@ -4,178 +4,65 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace study10
+namespace Bingo
 {
     class Program
     {
         static void Main(string[] args)
         {
-
-            //int[] num = new int[3];
-
-            //num[0] = 10;
-            //num[1] = 20;
-            //num[2] = 30;
-
-            ////Console.WriteLine(num[0]);
-            ////Console.WriteLine(num[1]);
-
-
-
-            //for (int i = 0; i < 3; i++)
-            //{
-            //    Console.WriteLine(num[i]);
-            //}
-
-            //int[] numbers = { 1, 2, 3 };
-            //int[] numbers2 = new int[3];
-            //int[] numbers3 = new int[] { 1, 2, 3 };
-
-            //string[] fruits = { "사과", "바나나", "오렌지" };
-
-            //for (int i = 0; i < 3; i++)
-            //{
-            //    Console.WriteLine(fruits[i]);
-            //}
-
-
-
-
-            //int[] kor = new int[3];
-            //int[] eng = new int[3];
-            //int[] math = new int[3];
-
-            //int[] sum = new int[3];
-            //float[] aver = new float[3];
-
-
-
-            //for (int i = 0; i < 3; i ++)
-            //{
-            //    Console.WriteLine("학생의 성적을 순서대로 입력하시오: ");
-            //    Console.Write("국어: ");
-            //    kor[i] = int.Parse(Console.ReadLine());
-            //    Console.Write("영어: ");
-            //    eng[i] = int.Parse(Console.ReadLine());
-            //    Console.Write("수학: ");
-            //    math[i] = int.Parse(Console.ReadLine());
-
-            //    sum[i] = kor[i] + eng[i] + math[i];
-
-            //    aver[i] = sum[i] / 3;
-
-            //}
-
-
-            //for(int i = 0; i < 3; i++)
-            //{
-            //    Console.WriteLine("1번 학생");
-            //    Console.WriteLine($"국어: {kor[i]}, 영어: {eng[i]}, 수학: {math[i]}");
-            //    Console.WriteLine("총점 : " + sum[i]);
-            //    Console.WriteLine("평균 : " + aver[i].ToString("F2"));
-            //}
-
-            //int[] scores = new int[3];
-
-            //scores[0] = 90;
-            //scores[1] = 85;
-            //scores[2] = 88;
-
-            //for (int i = 0; i < scores.Length; i++)
-            //{
-            //    Console.WriteLine($"점수 {i + 1}:{scores[i]}        ");
-            //}
-
-            /*double value = 123.456789;
-
-            Console.WriteLine(value.ToString("F2"));
-            Console.WriteLine($"소수점둘째자리:{value:F2}");*/
-
-            //double value = 12314123.14124;
-
-            //Console.Write(value.ToString("N2"));
-
-            //int[,] matrix = new int[2, 3]
-            //{
-            //    {1,2,3 }, {4,5,6}
-            //};
-
-            //for(int i =0; i < 2; i++)
-            //{
-
-
-            //    for(int j = 0; j < 3; j++)
-            //    {
-            //        Console.Write($"{matrix[i, j]}");
-            //    }
-            //}
-
-            //int a = 10;
-            //int b = 20;
-            //int t = 0;
-            //t = a;
-            //a = b;
-            //b = t;
-
             int[] iArray = new int[25];
 
-            for(int i=0; i<25; i++)
+
+            for (int i = 0; i < 25; i++)
             {
                 iArray[i] = i + 1;
             }
 
 
-            for (int i = 0; i < 5; i++)
-            {
-                for (int j = 0; j < 5; j++)
-                {
-                    Console.Write(iArray[i * 5 + j].ToString("D2") + " ");
-                }
-                Console.WriteLine();
-            }
 
             Random rand = new Random();
-
-            for(int i = 0; i < 100; i++)
+            //셔플
+            for (int i = 0; i < 100; i++)
             {
                 int iA = rand.Next(0, 25);
                 int iB = rand.Next(0, 25);
                 int iT = 0;
 
+
                 iT = iArray[iA];
                 iArray[iA] = iArray[iB];
                 iArray[iB] = iT;
-
             }
-            Console.WriteLine("---------------------------");
 
             int input = 0;
 
+            int iCount = 0;
+            int iBingo = 0;
             while (true)
             {
                 Console.Clear();
 
+                //빙고판
                 for (int i = 0; i < 5; i++)
                 {
                     for (int j = 0; j < 5; j++)
                     {
 
-                        if (iArray[i * 5 + j] == 0)
+                        if (iArray[i * 5 + j] == 0)  //0일경우 체크
                         {
-                            Console.Write(" *");
+                            Console.Write(" * ");
                         }
                         else
                         {
                             Console.Write(iArray[i * 5 + j].ToString("D2") + " ");
-
                         }
-
                     }
                     Console.WriteLine();
                 }
+                Console.WriteLine("빙고 숫자 : " + iBingo);
                 Console.WriteLine("숫자를 입력하세요: ");
                 input = int.Parse(Console.ReadLine());
-
+                iBingo = 0;
                 for (int i = 0; i < 25; i++)
                 {
                     if (iArray[i] == input)
@@ -185,7 +72,123 @@ namespace study10
                     }
                 }
 
+
+                //빙고 체크 로직
+                //가로 체크
+                for (int i = 0; i < 5; ++i)
+                {
+                    for (int j = 0; j < 5; ++j)
+                    {
+                        if (iArray[i * 5 + j] == 0)
+                        {
+                            ++iCount;
+                        }
+
+                        if (iCount == 5)
+                        {
+                            ++iBingo;
+                        }
+                    }
+                    iCount = 0;
+                }
+
+                //세로 체크
+                for (int i = 0; i < 5; i++)
+                {
+                    for (int j = 0; j < 5; j++)
+                    {
+                        if (iArray[i + 5 * j] == 0)
+                        {
+                            ++iCount;
+                        }
+
+                        if (iCount == 5)
+                        {
+                            ++iBingo;
+                        }
+                    }
+                    iCount = 0;
+                }
+
+
+                //대각선 오른쪽체크
+                //00 01 02 03 04
+                //05 06 07 08 09
+                //10 11 12 13 14
+                //15 16 17 18 19
+                //20 21 22 23 24
+
+                for (int i = 0; i < 5; i++)
+                {
+                    if (iArray[i * 4 + 4] == 0)
+                    {
+                        ++iCount;
+                    }
+
+                    if (iCount == 5)
+                    {
+                        ++iBingo;
+                    }
+                }
+                iCount = 0;
+
+
+                //대각선 왼쪽체크
+                //00 01 02 03 04
+                //05 06 07 08 09
+                //10 11 12 13 14
+                //15 16 17 18 19
+                //20 21 22 23 24
+                for (int i = 0; i < 5; i++)
+                {
+                    if (iArray[i * 6] == 0)
+                    {
+                        ++iCount;
+                    }
+
+                    if (iCount == 5)
+                    {
+                        ++iBingo;
+                    }
+                }
+                iCount = 0;
+
+
+                if (iBingo >= 5)
+                {
+                    Console.WriteLine("빙고 성공");
+                    break;
+                }
             }
+
+
+
+            int[,] board = new int[5, 5];
+            bool[,] marked = new bool[5,5];
+
+            int bingoCount = 0;
+
+            Random random = new Random();
+
+            int[] numbers = new int[25];
+
+            for (int i = 0; i < 25; i++)
+                numbers[i] = i + 1;
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
