ec5b210cae696f5680b545542a506608d26ecd72
jaejae1205 <163630615+jaejae1205@users.noreply.github.com>
2025-02-27 18:06:58 +0900

    2.27

---
+++ Inventory2/Inventory2/Program.cs
@@ -0,0 +1,136 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Inventory2
+{
+
+    struct Item
+    {
+        public string Name;
+        public int Count;
+
+        public Item(string name, int count)
+        {
+            Name = name;
+            Count = count;
+        }
+    }
+    class Program
+    {
+
+        const int MAX_ITEMS = 10;
+
+        static Item[] inventory = new Item[MAX_ITEMS];
+
+        static void AddItem(string name, int count)
+        {
+            for(int i =0; i<MAX_ITEMS; i++)
+            {
+                if (inventory[i].Name == name)
+                {
+                    inventory[i].Count += count;
+                    return;
+                }
+
+            }
+
+            for (int i = 0; i < MAX_ITEMS; i++)
+            {
+                if (inventory[i].Name == null)
+                {
+                    inventory[i].Name = name;
+                    inventory[i].Count = count;
+                    return;
+                }
+
+            }
+            Console.WriteLine("인벤토리가 가득 찼습니다.");
+
+        }
+
+
+        static void RemoveItem(string name, int count)
+        {
+            for(int i =0; i<MAX_ITEMS; i++)
+            {
+                if (inventory[i].Name == name)
+                {
+                    if (inventory[i].Count >= count)
+                    {
+                        inventory[i].Count -= count;
+                        if (inventory[i].Count == 0)
+                        {
+                            inventory[i].Name = null;
+                        }
+                        return;
+                    }
+                    else
+                    {
+                        Console.WriteLine("아이템 개수가 부족합니다!");
+                        return;
+                    }
+                }
+
+
+            }
+
+            Console.WriteLine("아이템을 찾을 수 없습니다");
+
+
+        }
+
+        static void ShowInventory()
+        {
+            Console.WriteLine("현재 인벤토리 : ");
+            bool isEmpty = true;
+
+            for (int i = 0; i < MAX_ITEMS; i++)
+            {
+                if (inventory[i].Name != null)
+                {
+                    Console.WriteLine($"{inventory[i].Name} (x{inventory[i].Count})");
+                    isEmpty = false;
+                }
+            }
+
+            if (isEmpty)
+            {
+                Console.WriteLine("인벤토리가 비어 있습니다.");
+            }
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            //테스트 : 아이템 추가
+            AddItem("포션", 5);
+            AddItem("칼", 1);
+            AddItem("포션", 3); //포션 개수 추가
+
+            ShowInventory();
+
+            //아이템 사용
+            Console.WriteLine("포션 2개 사용");
+            RemoveItem("포션", 2);
+            ShowInventory();
+
+            //테스트 : 없는 아이템 제거
+            Console.WriteLine("방패 1개 제거 시도");
+            RemoveItem("방패", 1);
+
+            ShowInventory();
+
+
+            //테스트: 모든 포션 제거
+            Console.WriteLine("포션 6개 사용(초과 사용 테스트)");
+            RemoveItem("포션", 6);
+            ShowInventory();
+
+
+        }
+    }
+}
---
+++ ShootingGame2/ShootingGame2/Program.cs
@@ -0,0 +1,97 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace ShootingGame1
+{
+    struct Player
+    {
+        public int X;
+        public int Y;
+        public string[] player;
+
+        public Player(int x, int y)
+        {
+            X = x;
+            Y = y;
+            player = new string[]
+            {
+                "->",
+                ">>>",
+                "->",
+            };
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.SetWindowSize(80, 25); 
+            Console.SetBufferSize(80, 25); 
+
+
+            ConsoleKeyInfo keyInfo;
+
+            Player player = new Player(0, 12);
+
+            Console.CursorVisible = false;
+
+            Stopwatch stopwatch = new Stopwatch();
+            stopwatch.Start();
+
+            long prevSecond = stopwatch.ElapsedMilliseconds;
+
+
+
+
+            while (true)
+            {
+
+                long currentSecond = stopwatch.ElapsedMilliseconds; 
+
+
+                if (currentSecond - prevSecond >= 100)
+                {
+
+                    Console.Clear();
+
+                    if (Console.KeyAvailable) //키가 눌렸을때 true
+                    {
+                        keyInfo = Console.ReadKey(true); //키 입력 받기 (화면 출력 X)
+                                                         //방향키 입력에 따른 좌표 변경
+                        switch (keyInfo.Key)
+                        {
+                            case ConsoleKey.UpArrow: if (player.Y > 0) player.Y--; break;
+                            case ConsoleKey.DownArrow: if (player.Y < Console.WindowHeight - 1) player.Y++; break;
+                            case ConsoleKey.LeftArrow: if (player.X > 0) player.X--; break;
+                            case ConsoleKey.RightArrow: if (player.X < Console.WindowWidth - 1) player.X++; break;
+                            case ConsoleKey.Spacebar: Console.Write("미사일키"); break;
+                            case ConsoleKey.Escape: return; //ESC키로 종료 
+
+                        }
+
+                    }
+
+
+                    for (int i = 0; i < player.player.Length; i++)
+                    {
+                        //콘솔좌표 설정 플레이어X 플레이어Y
+                        Console.SetCursorPosition(player.X, player.Y + i);
+                        //문자열배열 출력
+                        Console.WriteLine(player.player[i]);
+                    }
+
+
+
+                    prevSecond = currentSecond;//이전 시간 업데이트
+                }
+
+            }
+
+        }
+    }
+}
